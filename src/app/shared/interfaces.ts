706c3e960b8d6733b273ec3874f2c362bf657ca1
Sergii Antropov <homoantropos@gmail.com>
2021-07-20 16:12:22 +0300

    interfaces are created

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -3,24 +3,51 @@ export interface Aim {
   description: string;
   start: Date;
   finish: Date;
-  state: Array<Way>;
+  state: State;
   owner: User;
+  id?: number;
+}
+
+export interface State {
+  ways: Array<Way>;
+  readonly created: TimeStamp;
+  updated: TimeStamp;
+  id?: number;
 }
 
 export interface Way {
   direction: string;
-  mileStones: Array<MileStone>;
-  driver: User;
+  milestones: Array<Milestone>;
+  leader: User;
+  id?: number;
 }
 
-export interface MileStone {
+export interface Milestone {
   deadLine: Date;
+  tasks: Array<Task>;
+  readonly created: TimeStamp;
+  updated: TimeStamp;
+  id?: number;
+}
 
+export interface Task {
+  todo: string;
+  deadLine: Date;
+  operator: User;
+  operators: Array<User>;
+  readonly created: TimeStamp;
+  updated: TimeStamp;
+}
+
+export interface TimeStamp {
+  readonly createdAt: Date;
+  readonly createdBy: User;
 }
 
 export interface User {
   email: string;
   password: string;
+  phone: string;
   role: string;
   id?: number;
 }
